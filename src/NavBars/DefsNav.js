0651e066dce3a9213b32d5815a4c3b3c256a64fa
Quentin Bourgeois <quentintgc@live.fr>
2022-01-28 02:27:28 +0100

    first day

--- src/NavBars/DefsNav.js
+++ src/NavBars/DefsNav.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function DefsNav() {
   return (
       <nav className='defs-nav'>
-          <Link to="/definitions/nft">NFT</Link>
-          <Link to="/definitions/digital-asset">Digital Asset</Link>
-          <Link to="/definitions/blockchain">Blockchain</Link>
+          <Link to="/nudge-nft/definitions/nft">NFT</Link>
+          <Link to="/nudge-nft/definitions/digital-asset">Digital Asset</Link>
+          <Link to="/nudge-nft/definitions/blockchain">Blockchain</Link>
       </nav>
   );
 }
--- src/NavBars/MainNav.js
+++ src/NavBars/MainNav.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function MainNav() {
   return (
       <nav className='main-nav'>
-        <Link to="/">Home</Link>
-        <Link to="/definitions/nft">What's NFT ?</Link>
-        <Link to="/quizz">Quizz</Link>
+        <Link to="/nudge-nft/">Home</Link>
+        <Link to="/nudge-nft/definitions/nft">What's NFT ?</Link>
+        <Link to="/nudge-nft/quizz">Quizz</Link>
       </nav>
   );
 }
